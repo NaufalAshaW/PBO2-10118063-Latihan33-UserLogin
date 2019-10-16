b67d919062486129a230ac91b814cee00aaf63d6
Naufal Asha <55681676+NaufalAshaW@users.noreply.github.com>
2019-10-16 19:00:39 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118063/latihan33/Latihan33Login.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118063.latihan33;
+
+import java.util.Scanner;
+/**
+ *
+ * @author 
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+public class Latihan33Login {
+    
+        private static String usName, passWord;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        User user = new User();
+        
+        System.out.print("Masukkan Username = ");
+        usName = scanner.next();
+        
+        System.out.print("Masukkan Password = ");
+        passWord = scanner.next();
+        
+        user.pengecekkanLogin(usName, passWord);
+    }
+    
+}
